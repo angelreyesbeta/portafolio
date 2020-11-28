6676359993c7b866038e5bfa08d9ea578e6dafa3
Angel Reyes <angelreyesbeta@hotmail.com>
2020-11-28 00:07:47 -0500

    Componentes creados

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { About } from './components/about/About'
+import { Contactus } from './components/contactus/Contactus'
+import { Footer } from './components/footer/Footer'
+import { Header } from './components/header/Header'
+import { Portafolio } from './components/portafolio/Portafolio'
+import { Resume } from './components/resume/Resume'
+import { Testimoneals } from './components/testimoneals/Testimoneals'
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header/>
+        <About/>
+        <Resume/>
+        <Portafolio/>
+        <Testimoneals/>
+        <Contactus/>
+        <Footer/>
+
     </div>
-  );
+  )
 }
 
-export default App;
